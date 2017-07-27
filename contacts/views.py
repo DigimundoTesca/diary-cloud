438387297d9a03ad4591ce83565be71e207b91d2
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-07-27 16:46:12 -0500

    Añadidos todos los contactos en frontend

--- contacts/views.py
+++ contacts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .models import Contacto
+from .models import Contacto, Telefono
 
 
 def contactos(request):
     template = 'contactos.html'
     contactos_lista = Contacto.objects.all()
+    telefonos_lista = Telefono.objects.all()
     context = {
         'contactos': contactos_lista,
+        'telefonos': telefonos_lista
     }
     return render(request, template, context)
