17397c855cf67b7bc994a0c7da5e7440f413fa5c
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-07-27 16:49:46 -0500

    agregado validador de login para contactos

--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Contacto, Telefono
 
 
+@login_required()
 def contactos(request):
     template = 'contactos.html'
     contactos_lista = Contacto.objects.all()
