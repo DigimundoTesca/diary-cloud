c1a70808ac96b60fbc89c58f836ee3b6a364aaab
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-07-27 15:45:43 -0500

    Admin terminado

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+from .models import Contacto, Telefono
 
 
-# Register your models here.
+class TelefonoAdmin(admin.TabularInline):
+    model = Telefono
+    extra = 0
+
+
+@admin.register(Contacto)
+class ContactoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'empresa', 'telefono', 'nota')
+    inlines = (TelefonoAdmin, )
