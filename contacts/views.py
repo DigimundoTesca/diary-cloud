1d9e967eeab296e6e98f058b511cf241cd6d06d2
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-07-27 15:55:00 -0500

    Creada la primer pantalla para mostrar contactos

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Contacto
 
-# Create your views here.
+
+def contactos(request):
+    template = 'contactos.html'
+    contactos_lista = Contacto.objects.all()
+    context = {
+        'contactos': contactos_lista,
+    }
+    return render(request, template, context)
