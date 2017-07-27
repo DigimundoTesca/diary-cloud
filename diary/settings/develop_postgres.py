919913812b0676974b24249e6055956f04338d8e
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-07-27 14:27:02 -0500

    Changed requirements for develop in postgres

--- diary/settings/develop_postgres.py
+++ diary/settings/develop_postgres.py
@@ -7,10 +7,10 @@ from diary.settings.develop import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DABBANET_DB_NAME'),
-        'USER': os.getenv('DABBANET_DB_USER'),
-        'PASSWORD': os.getenv('DABBANET_DB_PASSWORD'),
-        'HOST': os.getenv('DABBANET_DB_HOST'),
-        'PORT': os.getenv('DABBANET_DB_PORT'),
+        'NAME': os.getenv('DIARY_DB_NAME'),
+        'USER': os.getenv('DIARY_DB_USER'),
+        'PASSWORD': os.getenv('DIARY_DB_PASSWORD'),
+        'HOST': os.getenv('DIARY_DB_HOST'),
+        'PORT': os.getenv('DIARY_DB_PORT'),
     }
 }
