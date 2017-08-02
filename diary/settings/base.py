d4a111d28ed33300abbf42adfd143b531eda5d67
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-08-01 22:02:24 -0500

    Added prod settings

--- diary/settings/base.py
+++ diary/settings/base.py
@@ -22,8 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DIARY_SECRET_KEY')
 
-ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
