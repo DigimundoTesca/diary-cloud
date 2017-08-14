f2f35c5530dd0a32dc89739785692143dd9c8071
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-08-13 20:07:44 -0500

    Modificados modelos

--- contacts/models.py
+++ contacts/models.py
@@ -22,20 +22,11 @@ class Empresa(models.Model):
         return '%s' % self.nombre
 
 
-      
-class Telefono(models.Model):
-    tipo = models.CharField(max_length=24, default='', help_text='Ingresa el tipo de telefono')
-    numero = models.CharField(max_length=12, default='', help_text='Ingresa un numero no mayor a 16 digitos')
-    contacto = models.ForeignKey(Contacto)
-
-    def __str__(self):
-        return '%s' % self.numero
-      
-
 class Contacto(models.Model):
     nombre = models.CharField(max_length=60, default='')
     telefono_principal = models.CharField(max_length=12, default='')
-    empresa = models.ForeignKey(Empresa)
+    empresa = models.CharField(max_length=255, null=True, blank=True)
+    empresa_fk = models.ForeignKey(Empresa, default=1)
     cargo = models.CharField(max_length=28, default='', blank=True, null=True)
     web = models.URLField(default='', blank=True, null=True)
     email = models.EmailField(default='', blank=True, null=True)
@@ -59,3 +50,11 @@ class Contacto(models.Model):
 
     telefono.allow_tags = True
 
+
+class Telefono(models.Model):
+    tipo = models.CharField(max_length=24, default='', help_text='Ingresa el tipo de telefono')
+    numero = models.CharField(max_length=12, default='', help_text='Ingresa un numero no mayor a 16 digitos')
+    contacto = models.ForeignKey(Contacto)
+
+    def __str__(self):
+        return '%s' % self.numero
