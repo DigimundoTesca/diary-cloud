9d50ca60da69c6bc0b068f20d836a25bbefe40a4
JBasilio24 <30379691+JBasilio24@users.noreply.github.com>
2018-03-08 14:07:08 -0600

    Update models.py

--- contacts/models.py
+++ contacts/models.py
@@ -172,7 +172,7 @@ class Contacto(models.Model):
     fecha_de_creacion = models.DateTimeField(editable=False, auto_now=True, null=True)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    telefono = models.CharField(validators=[phone_regex], max_length=17, blank=True, default='0', , null=True)
+    telefono = models.CharField(validators=[phone_regex], max_length=17, blank=True, default='0', null=True)
     extension = models.PositiveSmallIntegerField(blank=True, null=True)
     celular = models.CharField(validators=[phone_regex], max_length=17, blank=True, default='0')
     empresa = models.CharField(max_length=30, default='', blank=True, null=True)
